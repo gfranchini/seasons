9d11d5489f9ed44cb162d728bad3ff151e20fd6b
Giancarlo Franchini <gc@digistro.com>
2019-06-02 17:25:45 -0400

    Put our loading into a componentDedMount function

--- src/index.js
+++ src/index.js
@@ -11,28 +11,26 @@ class App extends React.Component {
     // This is a javascript object. We are initializing its own state. This is the
     // time we do direct assignment to this.state.
     this.state = { lat: 'loading...', errorMessage: '' };
+  }
 
+  componentDidMount(){
     // Getting user's location.
     window.navigator.geolocation.getCurrentPosition(
       //we called setState to update our position object. We did not do direct assignment!!!
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.Message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.Message })
     );
-  };
+  }
 
   // React says that we must define render!!
   render() {
     if(this.state.errorMessage && !this.state.lat) {
       return(<div>Error: {this.state.errorMessage}</div>);
-    };
+    }
 
     if(!this.state.errorMessage && this.state.lat){
       return(<div>Latitude: {this.state.lat}</div>);
-    };
+    }
 
     return(<div>Loading....</div>);
   };
