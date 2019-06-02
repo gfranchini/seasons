889ea09758afb3eb6a2168dad3ce36b297765cea
Giancarlo Franchini <gc@digistro.com>
2019-06-02 18:39:46 -0400

    detecting season from browser based on latitude and month

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
 
 // Class Component
 class App extends React.Component {
   // This is a javascript thing rather than a React thing. Like initializing a class in Ruby.
   // super overrides the React.Component constructor.
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
+  //
+  //   // This is a javascript object. We are initializing its own state. This is the
+  //   // time we do direct assignment to this.state.
+  //   this.state = { lat: 'loading...', errorMessage: '' };
+  // }
 
-    // This is a javascript object. We are initializing its own state. This is the
-    // time we do direct assignment to this.state.
-    this.state = { lat: 'loading...', errorMessage: '' };
-  }
+  // We are allowed to declare state without a constructor because babel does this for us. Remember the babels site example.
+  state = { lat: 'loading...'};
 
   componentDidMount(){
     // Getting user's location.
@@ -29,7 +34,7 @@ class App extends React.Component {
     }
 
     if(!this.state.errorMessage && this.state.lat){
-      return(<div>Latitude: {this.state.lat}</div>);
+      return(<SeasonDisplay lat={this.state.lat} />);
     }
 
     return(<div>Loading....</div>);
