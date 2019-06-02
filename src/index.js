73dae119223ed63f0b308ba321e5cc07ffeb96cf
Giancarlo Franchini <gc@digistro.com>
2019-06-02 11:18:49 -0400

    Added class constructor and initialized state

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Functional component
-// const App = () => {
-//   window.navigator.geolocation.getCurrentPosition(
-//     (position) => console.log(position),
-//     (err) => console.log(err)
-//   );
-//
-//   return(<div>Lattitude: </div>);
-// };
-
 // Class Component
 class App extends React.Component {
+  // This is a javascript thing rather than a React thing. Like initializing a class in Ruby.
+  // super overrides the React.Component constructor.
+  constructor(props) {
+    super(props);
+
+    // This is a javascript object. We are initializing its own state.
+    this.state = { lat: null };
+  };
+
+  // React says that we must define render!!
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
