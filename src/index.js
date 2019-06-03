aef8dff7230ccfa904667e369ba70d0af6d09524
Giancarlo Franchini <gc@digistro.com>
2019-06-02 22:01:34 -0400

    Added spinner component for loading

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-
-// Class Component
 class App extends React.Component {
-  // This is a javascript thing rather than a React thing. Like initializing a class in Ruby.
-  // super overrides the React.Component constructor.
-  // constructor(props) {
-  //   super(props);
-  //
-  //   // This is a javascript object. We are initializing its own state. This is the
-  //   // time we do direct assignment to this.state.
-  //   this.state = { lat: 'loading...', errorMessage: '' };
-  // }
 
   // We are allowed to declare state without a constructor because babel does this for us. Remember the babels site example.
   state = { lat: 'loading...'};
@@ -23,21 +13,21 @@ class App extends React.Component {
     window.navigator.geolocation.getCurrentPosition(
       //we called setState to update our position object. We did not do direct assignment!!!
       (position) => this.setState({ lat: position.coords.latitude }),
-      (err) => this.setState({ errorMessage: err.Message })
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
   // React says that we must define render!!
   render() {
     if(this.state.errorMessage && !this.state.lat) {
-      return(<div>Error: {this.state.errorMessage}</div>);
+      return <div>Error: {this.state.errorMessage}</div>;
     }
 
     if(!this.state.errorMessage && this.state.lat){
-      return(<SeasonDisplay lat={this.state.lat} />);
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return(<div>Loading....</div>);
+    return(<Spinner />);
   };
 };
 
